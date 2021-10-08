ce3408c27f989499fed7693dcee5bc95957ae863
Aviv Haruzi <avivharuzi@gmail.com>
2021-10-08 09:55:48 +0300

    feat: 🎸 add channel class with logic

---
+++ packages/mwc-browser/src/lib/channel-connection/index.ts
@@ -0,0 +1,4 @@
+export * from './channel-connection';
+export * from './channel-connection-creator';
+export * from './channel-connection-options';
+export * from './channel-connections-handler';
---
+++ packages/mwc-browser/src/lib/channel/channel-options.ts
@@ -0,0 +1,5 @@
+export interface ChannelOptions {
+  pingTimer: number;
+  zombiesTimer: number;
+  masterTimer: number;
+}
